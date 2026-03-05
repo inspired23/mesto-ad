bd637ff355ce87747811fb6e0459fa33a2a6b6c9
inspired23 <inspired2311@gmail.com>
2026-03-05 19:21:00 +0500

    commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,19 @@
-/*
-  Файл index.js является точкой входа в наше приложение
-  и только он должен содержать логику инициализации нашего приложения
-  используя при этом импорты из других файлов
+// файл index.js
+import { enableValidation, clearValidation } from "./components/validation.js";
 
-  Из index.js не допускается что то экспортировать
-*/
+// Создание объекта с настройками валидации
+const validationSettings = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+enableValidation(validationSettings);
 
 import { initialCards } from "./cards.js";
 import { createCardElement, deleteCard, likeCard } from "./components/card.js";
